2172d215966803292155ff36c5abf4dde0044b54
Toe Oo <tpso.toepyaesoneoo@gmail.com>
2020-10-24 16:18:56 +0630

    delete artists

--- src/views/admin/ArtistsManager/ArtistsManager.js
+++ src/views/admin/ArtistsManager/ArtistsManager.js
@@ -13,11 +13,16 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Pagination from '@material-ui/lab/Pagination'
 import InputAdornment from '@material-ui/core/InputAdornment'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogActions from '@material-ui/core/DialogActions'
 import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit'
 import SearchIcon from '@material-ui/icons/Search'
 import { LOAD_ADMIN_ARTISTS } from '../../../constants/actionTypes'
-import { find } from '../../../api/artists'
+import { find, remove } from '../../../api/artists'
 import { getTotalPages, getOffset, getIndex } from '../../../helpers/pagination'
 import useStyles from './ArtistsManagerStyle'
 
@@ -60,6 +65,8 @@ const ArtistsManager = ({
 
   const [page, setPage] = useState(1)
   const [search, setSearch] = useState('')
+  const [dialog, setDialog] = useState(false)
+  const [deleteId, setDeleteId] = useState(undefined)
 
   useEffect(() => {
     fetchArtists({}).then(loadArtists)
@@ -92,104 +99,148 @@ const ArtistsManager = ({
       .then(loadArtists)
   }
 
+  const confirmDelete = id => {
+    openDialog()
+    setDeleteId(id)
+  }
+
+  const handleDelete = () => {
+    remove(deleteId)
+      .then(() => fetchArtists({ name: search.trim() }))
+      .then(loadArtists)
+    setDeleteId(undefined)
+    closeDialog()
+  }
+
+  const closeDialog = () => setDialog(false)
+  const openDialog = () => setDialog(true)
+
   return (
-    <Card 
-      className={classes.root}
-      variant="outlined"
-    >
-      <Typography variant="h5">Artists</Typography>
-      <div 
-        className={classes.searchContainer}
+    <>
+      <Card 
+        className={classes.root}
+        variant="outlined"
       >
-        <TextField 
-          variant="outlined" 
-          label="Name"
-          size="small"
-          className={classes.search}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment positon="end">
-                <SearchIcon/>
-              </InputAdornment>
-            )
-          }}
-          value={search}
-          onChange={handleSearchChange}
-          onKeyDown={handleSearchKeyDown}
-        />
-        <Button 
-          variant="contained"
-          color="primary"
-          className={classes.searchButton}
-          disableElevation
-          onClick={handleSearch}
+        <Typography variant="h5">Artists</Typography>
+        <div 
+          className={classes.searchContainer}
         >
-          Search
-        </Button>
-      </div>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell className={classes.indexCol}>#</TableCell>
-              <TableCell className={classes.nameCol}>Name</TableCell>
-              <TableCell className={classes.actionCol}>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {!loading 
-              ? artists.map((artist, index) => 
-                  <TableRow key={artist.uuid}>
-                    <TableCell>
-                      {getIndex({ 
-                        page, 
-                        limit: LIMIT_PER_PAGE, 
-                        index,
-                      })}
-                    </TableCell>
-                    <TableCell
-                      className={classes.artistName}
-                    >
-                      {artist.name}
-                    </TableCell>
-                    <TableCell>
-                      <IconButton 
-                        size="small"
-                        className={classes.edit}
-                      >
-                        <EditIcon fontSize="inherit" />
-                      </IconButton>
-                      <IconButton 
-                        size="small"
-                        className={classes.delete}
-                      >
-                        <DeleteIcon fontSize="inherit" />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                )
-              : <TableRow>
-                  <TableCell 
-                    colSpan={3}
-                    className={classes.loadingCell}
-                  >Loading...</TableCell>
-                </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <div className={classes.paginationWrapper}>
-        {!loading && 
-          <Pagination
-            count={getTotalPages({ count, limit: LIMIT_PER_PAGE })} 
-            className={classes.pagination} 
-            color="primary"
-            page={page}
-            onChange={handlePageChange}
+          <TextField 
+            variant="outlined" 
+            label="Name"
+            size="small"
+            className={classes.search}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment positon="end">
+                  <SearchIcon/>
+                </InputAdornment>
+              )
+            }}
+            value={search}
+            onChange={handleSearchChange}
+            onKeyDown={handleSearchKeyDown}
           />
-        }
-      </div>
-    </Card>
+          <Button 
+            variant="contained"
+            color="primary"
+            className={classes.searchButton}
+            disableElevation
+            onClick={handleSearch}
+          >
+            Search
+          </Button>
+        </div>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.indexCol}>#</TableCell>
+                <TableCell className={classes.nameCol}>Name</TableCell>
+                <TableCell className={classes.actionCol}>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!loading 
+                ? artists.map((artist, index) => 
+                    <TableRow key={artist.uuid}>
+                      <TableCell>
+                        {getIndex({ 
+                          page, 
+                          limit: LIMIT_PER_PAGE, 
+                          index,
+                        })}
+                      </TableCell>
+                      <TableCell
+                        className={classes.artistName}
+                      >
+                        {artist.name}
+                      </TableCell>
+                      <TableCell>
+                        <IconButton 
+                          size="small"
+                          className={classes.edit}
+                        >
+                          <EditIcon fontSize="inherit" />
+                        </IconButton>
+                        <IconButton 
+                          size="small"
+                          className={classes.delete}
+                          onClick={() => confirmDelete(artist.uuid)}
+                        >
+                          <DeleteIcon fontSize="inherit" />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  )
+                : <TableRow>
+                    <TableCell 
+                      colSpan={3}
+                      className={classes.loadingCell}
+                    >Loading...</TableCell>
+                  </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <div className={classes.paginationWrapper}>
+          {!loading && 
+            <Pagination
+              count={getTotalPages({ count, limit: LIMIT_PER_PAGE })} 
+              className={classes.pagination} 
+              color="primary"
+              page={page}
+              onChange={handlePageChange}
+            />
+          }
+        </div>
+      </Card>
+      <Dialog 
+        open={dialog}
+        aria-labelledby="confirm-dialog-title"
+        aria-describedby="confirm-dialog-description"
+      >
+        <DialogTitle id="confirm-dialog-title">
+          Delete Confirmation
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="confirm-dialog-description">
+            Are you sure to delete?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog}>
+            Cancel
+          </Button>
+          <Button 
+            color="primary"
+            onClick={handleDelete}
+          >
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
